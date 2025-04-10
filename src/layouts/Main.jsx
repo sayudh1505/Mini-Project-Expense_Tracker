ffb69c8a2506e1b1295e0fcbbadbed1b49b58db2
sayudh1505 <sayudhrk1919@gmail.com>
2025-04-10 18:55:29 +0530

    final project

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,33 @@
+// rrd imports
+import { Outlet, useLoaderData } from "react-router-dom";
+
+// assets
+import wave from "../assets/wave.svg";
+
+// components
+import Nav from "../components/Nav";
+import { fetchData } from "../helpers";
+
+//  helper functions
+
+
+// loader
+export function mainLoader() {
+  const userName = fetchData("userName");
+  return { userName }
+}
+
+const Main = () => {
+  const { userName } = useLoaderData()
+
+  return (
+    <div className="layout">
+      <Nav userName={userName} />
+      <main>
+        <Outlet />
+      </main>
+      <img src={wave} alt="" />
+    </div>
+  )
+}
+export default Main
